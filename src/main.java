b1c19c200c40944523d243f2051fa9f83a451b81
faustinegl <faustine.galichet@edu.ece.fr>
2020-11-22 18:15:45 +0100

    mySQL init

--- src/main.java
+++ src/main.java
@@ -2,9 +2,9 @@ import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class FullDB {
+public class main {
     public static void main(String[] args) {
-        FullDB connection = new FullDB();
+        main connection = new main();
         connection.createConnection();
     }
 
@@ -14,32 +14,32 @@ public class FullDB {
             //Connect to Database
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connect = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:8889/Test","root","root");
+                    "jdbc:mysql://localhost:8889/Projet","root","root");
 
             //Create a sql statement
             Statement query = connect.createStatement();
 
             //Insert/Uodate data to Database
-            query.executeUpdate("INSERT INTO Test1" + "(nom)" + "VALUES('orange'), ('melons')," +
-                    "('hotdogs'), ('buns')");
+            //query.executeUpdate("INSERT INTO Table" + "(value)" + "VALUES('x'), ('y')," +
+            //        "('z'), ('a')");
 
             //Execute a select all statement
-            ResultSet rs = query.executeQuery("SELECT * FROM Test1");
-            System.out.println("Test");
-            while (rs.next()){
-                String table = rs.getString("nom");
-                System.out.println(table);
-            }
+            //ResultSet rs = query.executeQuery("SELECT * FROM Test1");
+            //System.out.println("Projet");
+//            while (rs.next()){
+//                String table = rs.getString("nom");
+//                System.out.println(table);
+//            }
 
 //            System.out.println("Connected to Database");
         }
 
         catch (ClassNotFoundException ex){
-            Logger.getLogger(FullDB.class.getName()).log(Level.SEVERE,null,ex);
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE,null,ex);
         }
 
         catch(SQLException ex){
-            Logger.getLogger(FullDB.class.getName()).log(Level.SEVERE,null,ex);
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE,null,ex);
             ex.printStackTrace();
         }
     }
